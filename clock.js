ca368e44f971c77a4e9b5aa7c63385aebb3b5b73
JRgerv <jack.gervais@gmail.com>
2017-02-03 08:10:50 -0800

    1st commit

---
+++ clock.js
@@ -0,0 +1,14 @@
+  var hands = [
+    {
+      hand: 'hours',
+      angle: (hours * 30) + (minutes / 2)
+    },
+    {
+      hand: 'minutes',
+      angle: (minutes * 6)
+    },
+    {
+      hand: 'seconds',
+      angle: (seconds * 6)
+    }
+  ];
